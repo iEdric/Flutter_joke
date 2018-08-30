1b183ec052e4d663feeb168d55c487d17dc1afff
edric <1158737828@qq.com>
2018-08-30 13:55:31 +0800

    update

--- flutter_joke/lib/Api/Api.dart
+++ flutter_joke/lib/Api/Api.dart
@@ -1,6 +1,6 @@
 
 class Api {
-
-  static final String MainURL = "http://v.juhe.cn/joke/content/text.php?key=5f88283a4a151160aa11bcb0dee0a0cf";
+  //
+  static final String MainURL = "http://v.juhe.cn/joke/content/text.php?key=XXXXXXX";
 
 }
